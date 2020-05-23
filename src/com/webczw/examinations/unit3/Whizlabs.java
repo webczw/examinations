53e0547a97b4d86d8f126e46417521db70dddfb6
webczw <webczw@163.com>
2020-05-24 07:13:18 +0800

    add Whizlabs files

---
+++ src/com/webczw/examinations/unit3/Whizlabs.java
@@ -0,0 +1,10 @@
+package com.webczw.examinations.unit3;
+
+public class Whizlabs {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("1Z0");
+        // sb.concat("-808");
+        System.out.println(sb);
+    }
+
+}
